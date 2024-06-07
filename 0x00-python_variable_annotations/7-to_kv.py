1427e7f128a5040f20726698f9334f23827b3fbb
MemanAwad <meman1awad@gmail.com>
2024-06-07 21:42:35 +0300

    string and number to tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@ from typing import Tuple
 from typing import Union
 
 
-def to_kv(k: str, v:[Union[int, float]]) -> Tuple[str, float]:
+def to_kv(k: str, V:[Union[int, float]]) -> Tuple[str, float]:
     """takes a list of integers and floats and returns their sum as a float"""
-    return [k, float(v**2)]
+    return [k, float(V**2)]
