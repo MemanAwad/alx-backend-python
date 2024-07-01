1a183701041f7ea4308a729b30405ef2441272c3
MemanAwad <meman1awad@gmail.com>
2024-07-01 21:46:06 +0300

    patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,9 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
-
+from utils import (access_nested_map, get_json)
+from unittest.mock import patch, MagicMock
+import requests
 
 class TestAccessNestedMap(unittest.TestCase):
     '''class for testing the access nested map function'''
@@ -26,3 +27,20 @@ class TestAccessNestedMap(unittest.TestCase):
         '''test that a KeyError is raised for the wrong input'''
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    '''class for testing get_json function'''
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.get_json')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        '''function test that utils.get_json returns the expected result.'''
+        mock_response  = MagicMock()
+        mock_response.json.return_value = test_payload
+        mock_get.get.return_value = mock_response 
+        self.assertEqual(get_json(test_url), test_payload)
+        
